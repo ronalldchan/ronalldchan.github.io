d3965cfd83a343691d7d0fa44d125369237d7c7c
Ronald Chan <ronaldc.rcws@gmail.com>
2022-01-30 15:44:09 -0800

    fixed side nav bar to have text

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const navSlide = () => {
     const collapse = document.querySelector('.collapse');
     const nav = document.querySelector('.nav-link');
+    // const navLink = document.querySelectorAll('.navlink li');
 
     collapse.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
-    })
+    });
+
+    // navLink.forEach((link, index) => {
+    //     link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 2}s'
+    // })
 }
 
 const app = () => {
