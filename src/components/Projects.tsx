285ee6a2061e6be5b5f0c89240bb6b9cdfa59d0e
Ronald Chan <ronaldc.rcws@gmail.com>
2024-03-19 11:28:11 -0700

    added translink db image

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,22 +2,25 @@ import { Box, Container, Grid, Typography } from "@mui/material";
 import ProjectCard from "./ProjectCard";
 import ubcnotes from "../images/ubcnotes.png";
 import website from "../images/website.png";
+import dbproject from "../images/translinkdb.png";
 
 const cardData = [
   {
+    title: "File Organizer DSL · 2024",
+    description: ["A DSL made to help users organize their files"],
+    image: ubcnotes,
+  },
+  {
     title: "UBC Notes · 2023",
     description: [
-      "A 12-hour hackathon project aimed at promoting inclusivity by creating a centralized platform for seamless, collaborative access to class notes among students.",
+      "A 12-hour nwPlus hackathon project aimed at promoting inclusivity by creating a centralized platform for seamless, collaborative access to class notes among students.",
       "Utilized Google Firebase to create the backend infrastructure, allowing for note storage and retrieval for knowledge exchange within the academic community.",
     ],
     image: ubcnotes,
   },
   {
     title: "Portfolio Website · 2023",
-    description: [
-      "My website that you are looking at now!",
-      "Created with TypeScript, React, and Material UI for an intuitive user interface",
-    ],
+    description: ["My website that you are looking at now!", "Created with TypeScript, React, and Material UI"],
     image: website,
   },
   {
@@ -29,11 +32,11 @@ const cardData = [
     image: website,
   },
   {
-    title: "TransLink Database App",
+    title: "TransLink Database App · 2021",
     description: [
       "A SQL database based on the TransLink transportation network using HTML and PHP to query, add, and remove data from the database.",
     ],
-    image: website,
+    image: dbproject,
   },
 ];
 
