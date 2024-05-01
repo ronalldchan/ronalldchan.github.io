b4b9c9937d832aacb32526366589807248cb06da
Ronald Chan <ronaldc.rcws@gmail.com>
2024-05-01 16:27:35 -0700

    remove timeline

--- vite-project/src/components/Experience.tsx
+++ vite-project/src/components/Experience.tsx
@@ -50,48 +50,6 @@ function Experience() {
             <Step active>{/* <StepLabel StepIconProps={{ icon: "" }} /> */}</Step>
           </Stepper>
         </Box>
-        {/* <Timeline position="alternate">
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineConnector />
-              <TimelineDot />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>
-              <ExperienceCard
-                jobTitle={"Quality Assurance Engineer Co-op"}
-                company="Operto"
-                companyLink="https://operto.com/"
-                description={[
-                  "Enabled E2E test automation for Operto Teams and Connect products by building a testing framework using Playwright and Typescript, allowing future test development.",
-                  "Automated new deployments by integrating the E2E test automation framework into the CI/CD pipeline, ensuring software stability.",
-                  "Developed an onboarding guide for QA engineers, ensuring a smooth integration process and effective orientation within the QA team.",
-                ]}
-                color="#000000"
-              />
-            </TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineConnector />
-              <TimelineDot />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>
-              <ExperienceCard
-                jobTitle={"Quality Assurance Engineer Co-op"}
-                company="Operto"
-                companyLink="https://operto.com/"
-                description={[
-                  "Enabled E2E test automation for Operto Teams and Connect products by building a testing framework using Playwright and Typescript, allowing future test development.",
-                  "Automated new deployments by integrating the E2E test automation framework into the CI/CD pipeline, ensuring software stability.",
-                  "Developed an onboarding guide for QA engineers, ensuring a smooth integration process and effective orientation within the QA team.",
-                ]}
-                color="#000000"
-              />
-            </TimelineContent>
-          </TimelineItem>
-        </Timeline> */}
       </Box>
     </Container>
   );
