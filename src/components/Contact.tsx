f49622c0431cdab022b7c17c7af7d8915ba13103
Ronald Chan <ronaldc.rcws@gmail.com>
2024-05-03 00:30:55 -0700

    remove import

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-import { Email, Phone } from "@mui/icons-material";
+import { Email } from "@mui/icons-material";
 import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
 
 function Contact() {
@@ -13,15 +13,6 @@ function Contact() {
           Contact
         </Typography>
         <Grid container justifyContent={"space-evenly"} py={3} spacing={3}>
-          {/* <Grid item>
-            <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
-              <Avatar sx={{ width: iconAvatar, height: iconAvatar, bgcolor: iconColor, mb: 1 }}>
-                <Phone sx={{ fontSize: iconSize }} />
-              </Avatar>
-              <Typography>Phone</Typography>
-              <Typography>604-307-6789</Typography>
-            </Box>
-          </Grid> */}
           <Grid item>
             <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
               <Avatar sx={{ width: iconAvatar, height: iconAvatar, bgcolor: iconColor, mb: 1 }}>
